71fd0affe1e80e561b247260da1f248719f2cf6c
tzaton <t.zaton@gmail.com>
2020-12-27 18:16:52 +0100

    add app website

---
+++ app/website-upload.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+
+BUCKET_NAME=$(cat setup/stack.yaml | cfn-flip | jq -r '.Parameters.AppBucket.Default')
+
+# Upload website
+aws s3api put-object \
+    --bucket "$BUCKET_NAME" \
+    --key index.html \
+    --body app/index.html \
+    --content-type text/html
--- setup/pre-stack.sh
+++ setup/pre-stack.sh
@@ -1,18 +1,31 @@
 #!/bin/bash
 set -e
 
-# Create bucket
-BUCKET_NAME=$(cat setup/stack.yaml | cfn-flip | jq -r '.Parameters.PreBucket.Default')
+# Create buckets
+PRE_BUCKET_NAME=$(cat setup/stack.yaml | cfn-flip | jq -r '.Parameters.PreBucket.Default')
+APP_BUCKET_NAME=$(cat setup/stack.yaml | cfn-flip | jq -r '.Parameters.AppBucket.Default')
 
-aws s3api create-bucket --bucket "$BUCKET_NAME"
+# technical ('pre')
+aws s3api create-bucket --bucket "$PRE_BUCKET_NAME"
 aws s3api put-public-access-block \
-    --bucket "$BUCKET_NAME" \
+    --bucket "$PRE_BUCKET_NAME" \
     --public-access-block-configuration "BlockPublicAcls=true,IgnorePublicAcls=true,BlockPublicPolicy=true,RestrictPublicBuckets=true"
 
-# Upload lambda function code
+# app
+aws s3api create-bucket --bucket "$APP_BUCKET_NAME"
+aws s3api put-public-access-block \
+    --bucket "$APP_BUCKET_NAME" \
+    --public-access-block-configuration "BlockPublicAcls=false,IgnorePublicAcls=false,BlockPublicPolicy=false,RestrictPublicBuckets=false"
+aws s3api put-bucket-policy --bucket "$APP_BUCKET_NAME" --policy file://app/policy.json
+
+# Upload website
+. app/website-upload.sh
+aws s3 website s3://"$APP_BUCKET_NAME"/ --index-document index.html --error-document error.html
+
+# Upload lambda functions
 . s3-to-ecs/lambda-upload.sh
 
-# Upload glue job code
+# Upload glue jobs
 . parse-xml/glue-job-upload.sh
 
 # Upload jars
@@ -21,7 +34,7 @@ JAR_DIR=$(cat setup/stack.yaml | cfn-flip | jq -r '.Mappings.DirMap.jars.name')
 wget https://repo1.maven.org/maven2/com/databricks/spark-xml_2.11/0.11.0/spark-xml_2.11-0.11.0.jar
 
 aws s3api put-object \
-    --bucket "$BUCKET_NAME" \
+    --bucket "$PRE_BUCKET_NAME" \
     --key  "$JAR_DIR"/spark-xml_2.11-0.11.0.jar \
     --body spark-xml_2.11-0.11.0.jar
 
