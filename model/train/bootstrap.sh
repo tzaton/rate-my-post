869b46b5d3986bb1cb2b112fa8433f4e7e57f961
tzaton <t.zaton@gmail.com>
2021-01-03 13:34:14 +0100

    added model notebooks

--- model/train/bootstrap.sh
+++ model/train/bootstrap.sh
@@ -2,5 +2,5 @@
 
 # EMR bootstrap action
 sudo mkdir /usr/lib/nltk_data
-sudo python3 -m nltk.downloader -d /usr/lib/nltk_data punkt averaged_perceptron_tagger universal_tagset vader_lexicon
-sudo python3 -m pip install pyarrow
+sudo python3 -m nltk.downloader -d /usr/lib/nltk_data punkt averaged_perceptron_tagger universal_tagset vader_lexicon stopwords
+sudo python3 -m pip install pyarrow==0.14 pandas==1.2.0 spark-nlp==2.6.2
